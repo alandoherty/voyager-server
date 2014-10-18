c5187f0d42d72e328172cbf5d041e2c67da075b4
Alan Doherty <alan@sth.im>
2014-10-18 20:42:14 +0100

    Added more data

---
+++ voyager-server/Data/HackathonData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace voyagerserver
+{
+	public struct HackathonData
+	{
+		#region Fields
+		[DataMember]
+		public string Name;
+
+		[DataMember]
+		public string Organizer;
+
+		[DataMember]
+		public string Image;
+
+		[DataMember]
+		public string Date;
+
+		//[DataMember]
+		//public LocationData Location;
+
+		[DataMember]
+		public string Location;
+		#endregion
+	}
+}
+
---
+++ voyager-server/Services/Eventbrite/EBCategory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace voyagerserver
+{
+	public struct EBCategory
+	{
+		#pragma warning disable 0169
+		[DataMember]
+		string resource_uri;
+
+		[DataMember]
+		int id;
+
+		[DataMember]
+		string name;
+
+		[DataMember]
+		string name_localized;
+
+		[DataMember]
+		string short_name;
+		#pragma warning restore 0169
+	}
+}
+
